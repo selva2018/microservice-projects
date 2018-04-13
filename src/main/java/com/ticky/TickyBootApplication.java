c2f11c34ba764d63877b2499fdcdf2f3e9ca24d5
selva268082 <selvapandy.m@gmail.com>
2018-04-13 08:20:33 -0400

    I added some comment

--- src/main/java/com/ticky/TickyBootApplication.java
+++ src/main/java/com/ticky/TickyBootApplication.java
@@ -2,7 +2,7 @@ package com.ticky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
+//Added
 @SpringBootApplication
 @EnableConfigurationProperties
 public class TickyBootApplication {
